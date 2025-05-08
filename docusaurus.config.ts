787d2da609a5440672a69fcc7fd8cf31e0115934
Xcelious <developer_daniel@icloud.com>
2025-05-07 19:55:48 -0700

    chore: change value in config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   projectName: "blacksky-algorithms.github.io",
   trailingSlash: false,
   deploymentBranch: "gh-pages",
-  onBrokenLinks: "throw",
+  onBrokenLinks: "log",
   onBrokenMarkdownLinks: "warn",
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
