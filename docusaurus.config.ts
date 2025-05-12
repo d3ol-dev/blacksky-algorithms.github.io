ccff92e4a9cf496ed9c953d03b1a5860f4e00cca
Dev Dan <developer_daniel@icloud.com>
2025-05-11 22:17:32 -0700

    bug: fixed broken link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -79,7 +79,7 @@ const config: Config = {
           items: [
             {
               label: "Blacksky Introduction",
-              to: "/docs/blacksky-intro",
+              to: "/docs",
             },
           ],
         },
